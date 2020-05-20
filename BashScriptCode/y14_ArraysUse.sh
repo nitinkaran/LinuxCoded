82dff3a9d7d7dd68dba3da532fbba27e30dfbc96
nitinkaran <nitinkaran31@gmail.com>
2020-05-20 22:28:11 +0530

    changed the error and added the new way to access and initialize the array

--- BashScriptCode/y14_ArraysUse.sh
+++ BashScriptCode/y14_ArraysUse.sh
@@ -3,10 +3,24 @@
 #THIS PROGRAM DEMONSTRATES THE USE OF ARRAYS IN A PROGRAM
 
 arrayA=("Varun" "Swapnil" "Karan" "Atul")
+
+
+
+arrayB=(ABC PQR CDE STU LMN)
+
 count=1
 
 for i in ${arrayA[*]}
 do
 	echo -e "The value at index = $count is : $i"
-	let count=count + 1
+	count=$((count + 1))
 done
+
+echo; echo;
+count=1
+for i in ${arrayB[*]}
+do
+	printf "The value at index = %d is : %s \n" $count $i
+	count=$((count + 1))
+done
+
