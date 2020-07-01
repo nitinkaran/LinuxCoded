59cc775d2b6af90895e3a18bbeeb9730900609c8
nitinkaran <nitinkaran31@gmail.com>
2020-07-01 19:44:22 +0530

    adding a comment that what does this file do

--- BashScriptCode/1_createMultipleCopyOfFiles.sh
+++ BashScriptCode/1_createMultipleCopyOfFiles.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-# CREATE 10 COPIES OF EACH .txt FILES IN THE CURRENT DIRECTORY
+# CREATE 10 COPIES OF EACH .txt FILES IN THE CURRENT DIRECTORY WITH A NEW FOLDER NAME CALLED AS
+# 'COPIED_FILES' WHICH WILL CONTAIN THE COPY OF EACH FILES 10 TIMES
 copy() {
 
-file=$1
+	file=$1
 
-echo -e $file
-
-for i in {1..10}
-do
-	cp $file "${file%.txt}_$i.txt"
-done
+	echo -e $file
+	
+	#CREATE A FOLDER IF IT DOESN'T EXIST
+	
+	directory="COPIED_FILES"
+	if [ -d "$directory" ] 
+	then
+		echo -e "DIRECTORY ALREADY EXIST \n"
+	else
+		printf "%s \n" "Creating a directory : \" $directory \""
+		mkdir $directory
+	fi
+	
+	
+	for i in {1..10}
+	do
+		cp $file ./$directory/"${file%.txt}_$i.txt"
+	done
 
 }
 
