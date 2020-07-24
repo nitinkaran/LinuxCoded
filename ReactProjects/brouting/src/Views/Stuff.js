5e7872d4a23f22129017dcfbeafc39cee26d2efa
NAGENDRA <nagendragithub2020@gmail.com>
2020-07-24 15:44:45 +0530

    routing added to the file

---
+++ ReactProjects/brouting/src/Views/Stuff.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Stuff = () => {
+    return (
+        <div className="home-component">
+            <h1 className="darker-heading">
+                Stuff PAGE
+            </h1>
+            <h4 className="faded-heading">
+                Just a small detail page need to be editted
+            </h4>
+        </div>
+    );
+};
+
+export default Stuff;
---
+++ ReactProjects/brouting/src/Views/SubTopic2.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SubTopic2 = () => {
+    return (
+        <div className="home-component">
+            <h1 className="darker-heading">
+                SubTopic2 Page
+            </h1>
+            <h4 className="faded-heading">
+                Just a small detail page need to be editted
+            </h4>
+        </div>
+    );
+};
+
+export default SubTopic2;
