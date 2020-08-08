f3a9467a89f44236612efb55bb6060d302d7107e
NAGENDRA <nagendragithub2020@gmail.com>
2020-08-08 23:39:45 +0530

    comments removed

--- ReactProjects/code_1_todo/src/TodoApp.js
+++ ReactProjects/code_1_todo/src/TodoApp.js
@@ -3,22 +3,6 @@ import { connect } from "react-redux";
 import { mapDispatchToProps, mapStateToProps } from "./Todo.connect";
 import propsProvider from "./Todo.propsProvider";
 import TodoAppMainView from "./Views/TodoApp.MainView";
-// import AddTodoComponent from './Views/AddTodo.Component';
-// import TodoList from "./Views/TodoList";
-// import Filters from "./Views/Filters";
-
-// const TodoApp = () => {
-//     return (
-//         <div className="a-todo-app">
-//             <div className="todo-title">Add Todos !!! </div>
-//             <AddTodoComponent />
-//             <TodoList />
-//             <Filters />
-//         </div>
-//     );
-// }
-
-// export default TodoApp;
 
 class TodoApp extends Component {
 
