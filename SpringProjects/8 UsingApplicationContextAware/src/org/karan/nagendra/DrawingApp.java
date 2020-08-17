32f1063cfd369d4a0c0ce795d6aca19421972fee
NAGENDRA <nagendragithub2020@gmail.com>
2020-08-17 21:06:03 +0530

    using the scope of bean creation and using ApplicationContextAware to get the bean before the parent component can use it and perform some functionality

---
+++ SpringProjects/8 UsingApplicationContextAware/src/org/karan/nagendra/DrawingApp.java
@@ -0,0 +1,19 @@
+package org.karan.nagendra;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApp {
+
+	public static void main(String[] args) {
+		
+//		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		
+		Triangle tr = (Triangle)context.getBean("triangle");
+		tr.draw(); 	
+		
+		
+	}
+}
