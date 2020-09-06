120b96299793045f60d9564ca0fca89957cb6131
NAGENDRA <nagendragithub2020@gmail.com>
2020-09-06 13:00:21 +0530

    Cascading of product group to product has been added to handle the delete of products from product group as parent table gets reflected in the child table product

--- SpringBootProjects/BackendApi/src/main/java/com/karan/controller/ProductGroupController.java
+++ SpringBootProjects/BackendApi/src/main/java/com/karan/controller/ProductGroupController.java
@@ -1,5 +1,11 @@
 package com.karan.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +16,30 @@ import com.karan.service.ProductGroupService;
 @RestController
 public class ProductGroupController {
 
+	@Autowired
 	private ProductGroupService productGroupService;
+	
 	@PostMapping(value = "/product-group")
-	public void createProductGroup(@RequestBody ProductGroup productGroup) {
+	public void createProductGroup(@RequestBody List<ProductGroup> productGroup) {
 		productGroupService.createProductGroup(productGroup);
+	}
+	
+	@GetMapping(value = "/product-group")
+	public List<ProductGroup> retrieveProductGroup() {
+		return productGroupService.retrieveProductGroup();
+	}
+	
+	@GetMapping(value = "/product-group/{type}")
+	public List<ProductGroup> retrieveProductGroupByType(@PathVariable String type) {
+		return productGroupService.retrieveProductGroupByType(type);
+	}
+	
+	
+	/*
+	 * Remove a product group and all its related products 
+	 */
+	@DeleteMapping(value = "/product-group/{type}")
+	public void removeProductGroup(@PathVariable String type) {
+		productGroupService.removeProductGroup(type);
 	}
 }
--- SpringBootProjects/BackendApi/src/main/java/com/karan/service/ProductGroupService.java
+++ SpringBootProjects/BackendApi/src/main/java/com/karan/service/ProductGroupService.java
@@ -1,5 +1,9 @@
 package com.karan.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +16,28 @@ public class ProductGroupService {
 	@Autowired
 	ProductGroupRepository productGroupRepository;
 	
-	public void createProductGroup(ProductGroup productGroup) {
-		productGroupRepository.save(productGroup);
+	public void createProductGroup(List<ProductGroup> productGroup) {
+		productGroupRepository.saveAll(productGroup);
 	}
 
-	
+	public List<ProductGroup> retrieveProductGroup() {
+		return productGroupRepository.findAll();
+	}
+
+	public List<ProductGroup> retrieveProductGroupByType(String type) {
+		List<ProductGroup> list = new ArrayList<ProductGroup>();
+		
+		((List<ProductGroup>)(productGroupRepository.findAll()))
+		.stream()
+		.filter(item -> item.getProductGroupType().equals(type))
+		.forEach(list::add);
+		
+		return list;
+	}
+
+	public void removeProductGroup(String type) {
+		productGroupRepository.deleteById(type);
+	}
+
 	
 }
