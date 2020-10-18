519acf64cdc775cb08157990fb924689b0b4c4b7
NAGENDRA <nagendragithub2020@gmail.com>
2020-10-18 23:52:04 +0530

    simple basic structure of SPA using react router

---
+++ ReactProjects/react_routing_projects/react-routing-1/src/Content/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            THIS IS THE Contact PAGE
+        </div>
+    );
+};
+
+export default Contact;
---
+++ ReactProjects/react_routing_projects/react-routing-1/src/Content/ReactFile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReactFile = () => {
+    return (
+        <div>
+            this is the REACT_FILE page to show the react life cycle
+        </div>
+    );
+};
+
+export default ReactFile;
